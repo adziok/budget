fe5a7ea6a35101c9ccaee6c0a1630647f102f0c1
Adrian Karwowski <adrian.karwowski@elpassion.pl>
2021-11-19 21:14:25 +0100

    feat: events sourcing prototype

---
+++ apps/backend/src/domain/Accounnt.spec.ts
@@ -0,0 +1,14 @@
+import { Account, AccountCreated, TransactionOccurred } from './Account';
+
+describe('Account', () => {
+    it('should capture 2 incomes and show valid money amount', () => {
+        const account = new Account();
+        [
+            new AccountCreated('My account', 100),
+            new TransactionOccurred(100, 'income'),
+            new TransactionOccurred(50, 'income'),
+        ].forEach((event) => account.apply(event));
+
+        expect(account['props'].money).toEqual(250);
+    });
+});
---
+++ apps/backend/src/domain/Account.ts
@@ -0,0 +1,36 @@
+export interface IAccountProps {
+    name: string;
+    money: number;
+}
+
+export interface IDomainEvent {
+    EVENT_TYPE: string;
+}
+
+export class AccountCreated implements IDomainEvent {
+    public EVENT_TYPE = 'AccountCreated';
+    constructor(public readonly name: string, public readonly initialMoneyAmount: number) {}
+}
+
+export class TransactionOccurred implements IDomainEvent {
+    public EVENT_TYPE = 'TransactionOccurred';
+    constructor(public readonly moneyAmount: number, public readonly type: 'expense' | 'income') {}
+}
+
+export class Account {
+    private props: IAccountProps;
+    constructor() {
+        this.props = {} as any;
+    }
+
+    apply(event: IDomainEvent) {
+        if (event instanceof AccountCreated) {
+            this.props.name = event.name;
+            this.props.money = event.initialMoneyAmount;
+        } else if (event instanceof TransactionOccurred) {
+            this.props.money = this.props.money + event.moneyAmount * ((event.type === 'expense' && -1) || 1);
+        } else {
+            throw new Error('Invalid event: ' + event?.EVENT_TYPE);
+        }
+    }
+}
