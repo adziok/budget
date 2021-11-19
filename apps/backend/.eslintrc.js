0bb858e19227710eb7fbd4c0beb0c60bea7cbb16
Adrian Karwowski <adrian.karwowski@elpassion.pl>
2021-11-19 16:41:50 +0100

    feat: configure eslint and prettier feat: add yarn workspaces config

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -20,5 +20,14 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': ["error", {allow: ["constructors"]}],
+    'require-await': 'error',
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+    ],
+    "@typescript-eslint/no-namespace": "off",
+    "@typescript-eslint/no-non-null-assertion": "off"
   },
 };
